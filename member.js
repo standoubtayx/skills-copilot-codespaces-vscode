99dc52bef25b72e01eb7f27c09aeafde0f9bd089
simon stratton <simon.stratton@alteryx.com>
2024-02-22 17:22:04 +0100

    Added member

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    const member = {
+        name: 'Kasim',
+        age: 25,
+        skills: ['JavaScript', 'React', 'Node', 'TypeScript'],
+        details: {
+            education: 'B.Tech',
+            address: 'Kerala'
+        }
+    }
+    return member;
+}
